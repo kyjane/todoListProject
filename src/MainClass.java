1d6a04391042b717370d21331f8c427571469a88
Ser3na <s.er3na.j@gmail.com>
2019-03-08 15:18:19 +0800

    commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream; 
+
+import java.util.stream.Collectors; 
+import java.util.Scanner;
 
 public class MainClass {
 	
@@ -9,13 +14,36 @@ public class MainClass {
 		list.add("read books 1 hour");
 		list.add("read news 1 hour");
 		showMenu(list);
+		
+		 selectItem(list);
+
+		
 	}
 	
 	public static void showMenu(ArrayList<String> list){
 		
 		for(int i=0; i<list.size();i++)
 		{
-		 System.out.println("To Do Item: " + list.get(i));
+		 System.out.println("To Do Item: Item " + (i)+ " "  + list.get(i));
 		}
 	}
+
+    
+    public static void selectItem(ArrayList list) //remove item from list
+    {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	boolean exit = false;
+    
+    	     
+    	            System.out.println("Select Item to remove: ");
+    	            sc = new Scanner(System.in);
+    	            int index = sc.nextInt();
+    	          
+    	        
+    	            list.remove(index); 
+    	            System.out.println("Remaining Listing: " + list);
+    	            
+    	            
+    }
 }
